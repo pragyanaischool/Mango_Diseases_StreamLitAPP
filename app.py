6d8f7b2f304908b9936e1bfb8b4be857aa28bfd2
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-12-05 19:54:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,18 +35,6 @@ with st.sidebar:
         st.subheader("Accurate detection of diseases present in the mango leaves. This helps an user to easily detect the disease and identify it's cause.")
 
              
-        
-def prediction_cls(prediction):
-    for key, clss in class_names.items():
-        if np.argmax(prediction)==clss:
-            
-            return key
-        
-       
-
-    
-
-st.set_option('deprecation.showfileUploaderEncoding', False)
 @st.cache(allow_output_mutation=True)
 def load_model():
     model=tf.keras.models.load_model('mango_model.h5')
